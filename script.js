2a815aca80c6f9f6acce8eae321cd2f35a9e6cb7
John <john@witchhat.co.uk>
2023-12-13 02:25:22 +0530

    <script.js> - Fixed logic error of negative initialWeight and age or zero weight - Modified to auto calculate, making calculate button redundant

--- script.js
+++ script.js
@@ -7,27 +7,44 @@ function calculateWeight() {
   const initialWeight = parseFloat(document.getElementById('initialWeight').value);
   const age = parseInt(document.getElementById('age').value);
 
-  // Set growth rate based on breed size
-  let growthRate;
+  // Check if all input fields are filled
+  if (breedSize !== '' && !isNaN(initialWeight) && !isNaN(age)) {
+    // Set growth rate based on breed size
+    let growthRate;
 
-  switch (breedSize) {
-    case 'small':
-      growthRate = 0.5;
-      break;
-    case 'medium':
-      growthRate = 1;
-      break;
-    case 'large':
-      growthRate = 1.5;
-      break;
-    default:
-      break;
+    switch (breedSize) {
+      case 'small':
+        growthRate = 0.5;
+        break;
+      case 'medium':
+        growthRate = 1;
+        break;
+      case 'large':
+        growthRate = 1.5;
+        break;
+      default:
+        break;
+    }
+
+    // Prevent Zero or Negative Logic Error
+    if (initialWeight > 0.00 && age > 0.00) {
+      // Calculate estimated weight
+      const estimatedWeight = initialWeight + growthRate * age;
+
+      // Display the result
+      const resultDiv = document.getElementById('result');
+      resultDiv.innerHTML = `<p>Estimated Weight at ${age} weeks: ${estimatedWeight.toFixed(2)} lbs</p>`;
+    }
+    
+    // Report Zero or Negative Logic Error
+  else {    
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = "<p>Can't have negative weight or age nor zero weight ;)</p>";
   }
-
-  // Calculate estimated weight
-  const estimatedWeight = initialWeight + growthRate * age;
-
-  // Display the result
-  const resultDiv = document.getElementById('result');
-  resultDiv.innerHTML = `<p>Estimated Weight at ${age} weeks: ${estimatedWeight.toFixed(2)} lbs</p>`;
+  }
 }
+
+// Attach input event listeners to each input field
+document.getElementById('breedSize').addEventListener('input', calculateWeight);
+document.getElementById('initialWeight').addEventListener('input', calculateWeight);
+document.getElementById('age').addEventListener('input', calculateWeight);
